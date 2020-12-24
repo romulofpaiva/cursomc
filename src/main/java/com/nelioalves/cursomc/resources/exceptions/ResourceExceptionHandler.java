858c71ed4e0b84b2719e6a3e79144891a3d73798
Romulo Ferreira Paiva <romulofpaiva@gmail.com>
2020-12-23 23:27:25 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @RestControllerAdvice
@@ -14,10 +15,16 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound( ObjectNotFoundException e, HttpServletRequest request ) {
-		
 		StandardError err = new StandardError( HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis() );
 		
 		return ResponseEntity.status( HttpStatus.NOT_FOUND ).body( err );
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity( DataIntegrityException e, HttpServletRequest request ) {
+		StandardError err = new StandardError( HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis() );
+		
+		return ResponseEntity.status( HttpStatus.BAD_REQUEST ).body( err );
 	}
 
 }
