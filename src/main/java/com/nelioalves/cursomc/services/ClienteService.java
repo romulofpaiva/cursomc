7d1397452c26a31398b8093b0b3fffb219a82148
Romulo Ferreira Paiva <romulofpaiva@gmail.com>
2020-12-29 23:11:56 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -53,7 +53,7 @@ public class ClienteService {
 		try {
 			repository.deleteById( id );
 		} catch( DataIntegrityViolationException e ) {
-			throw new DataIntegrityException( "Não é possível excluir pois há entidades relacionadas." );
+			throw new DataIntegrityException( "Não é possível excluir pois há pedidos relacionados." );
 		}
 	}
 }
